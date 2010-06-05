53501bcfa6fc2f10e555084dd58a3adb2fc63085
Andrew De Ponte <cyphactor@gmail.com>
2010-06-04 18:32:04 -0700

    Cleaned up the format_output.rb script and added exec perms.
    The py, rb, and sh scripts were lacking execution permissions so I added execution permissions to them for both the user and the group.

--- Support/format_output.rb
+++ Support/format_output.rb
@@ -1,31 +1,15 @@
 #!/usr/bin/env ruby -w
 
+# Handle all the propery formatting so that when command output is displayed
+# it is styled appropriately by TextMate.
+
 STDOUT.sync = true
 
 puts "<pre>"
-#puts "<script type=\"text/javascript\">"
-#puts "var newDiv = document.createElement(\"div\");"
-#puts "var objDiv = document.getElementById(\"divExample\");"
-#puts "newDiv.innerHTML = \"<pre>\""
-#puts "objDiv.appendChild(newDiv);"
-#puts "</script>"
 
 while line = STDIN.gets
   corrected_line = line.gsub("\n", "<br>")
   print corrected_line
-  #puts "<script type=\"text/javascript\">"
-  #puts "var newDiv = document.createElement(\"div\");"
-  #puts "var objDiv = document.getElementById(\"divExample\");"
-  #puts "newDiv.innerHTML = \"" + corrected_line + "\""
-  #puts "objDiv.appendChild(newDiv);"
-  #puts "objDiv.scrollTop = objDiv.scrollHeight;"
-  #puts "</script>"
 end
-
-#puts "<script type=\"text/javascript\">"
-#puts "newDiv.innerHTML = \"</pre>\""
-#puts "objDiv.appendChild(newDiv);"
-#puts "objDiv.scrollTop = objDiv.scrollHeight;"
-#puts "</script>"
 
 puts "</pre>"
